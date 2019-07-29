a5f8cc5dbf8c23491cecc3d5dc8f60df252a79be
Sajo8 <andrewsplin@protonmail.com>
2019-07-28 17:29:07 -0700

    Add license comments

--- src/main/java/tcj/Market.java
+++ src/main/java/tcj/Market.java
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2018 Sajo8
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see .
+*/
+
 package tcj;
 
 // To do GET requests
@@ -5,7 +22,9 @@ import kong.unirest.Unirest;
 import kong.unirest.JsonNode;
 import kong.unirest.HttpResponse;
 
+// To format numbers appropriately
 import java.text.NumberFormat;
+import java.text.DecimalFormat;
 
 // To parse json
 import org.json.JSONArray;
@@ -13,8 +32,6 @@ import org.json.JSONObject;
 
 // To color message
 import tcj.Utils;
-
-import java.text.DecimalFormat;
 
 public class Market {
     
@@ -24,7 +41,6 @@ public class Market {
         
         System.out.println(utils.colorLine("\nReceiving market stats...", "yellow"));
         NumberFormat nfScientific = new DecimalFormat("################################################.###########################################");
-        NumberFormat nfUSD = NumberFormat.getCurrencyInstance();
         NumberFormat nf = NumberFormat.getInstance();
 
         // Get data
@@ -35,9 +51,13 @@ public class Market {
         // Get the market data dict
         JSONObject marketData = actualJSON.getJSONObject(0);
         
+        
+        // Get USD price
         Double usdPriceScientific = marketData.getDouble("current_price");
         String usdPrice = "$" + nfScientific.format(usdPriceScientific);
 
+        
+        // Get 24h price change
         Double h24_price_change_double = marketData.getDouble
         ("price_change_percentage_24h");
         String h24_price_change = String.valueOf(h24_price_change_double);
@@ -46,10 +66,14 @@ public class Market {
         // If it's negative change, make it red. Else, make it green.
         h24_price_change = (h24_price_change.startsWith("-")) ? utils.colorLine(h24_price_change, "red") : utils.colorLine(h24_price_change, "green");
 
+        
+        // Get circulating supply
         Double circ_supply_double = marketData.getDouble("circulating_supply");
         String circ_supply = String.valueOf(circ_supply_double);
         circ_supply = nf.format(circ_supply_double) + " TRTL";
 
+        
+        // Format a string to return
         String marketDataString = "\n"
             + "Current USD Price: " + usdPrice
             + "\n"
